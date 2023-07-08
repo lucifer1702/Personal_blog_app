99fc6f63a64a0fe7d20e0f9722863ebabb25bd65
Mukund <mukund@Mukunds-MacBook-Air.local>
2023-07-08 21:48:00 +0530

    the second commit

--- my-blog-app/app/tests.py
+++ my-blog-app/app/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
-
+from app.models import Post
 # Create your tests here.
+from django.shortcuts import render
+
+def post_page(request,slug):
+    post=Post.objects.get(slug=slug)
+    context={'post':post}
+    return render(request,'app/post.html',)
+
